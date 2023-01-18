79ce41fed067af4587a826d6939b78ce1663bbc0
arnolb <arnolb555@.com>
2023-01-18 14:47:12 -0400

    implementacion de livewire

---
+++ app/Http/Livewire/Product.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Product extends Component
+{
+    public $text = 'Hola nene como me le va?';
+    public $sumar;
+
+    public function render()
+    {
+        return view('livewire.product');
+    }
+
+    public function button(){
+        $this->sumar++;
+    }
+}
---
+++ resources/views/livewire/product.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <h1>{{$text}}</h1>
+
+    <button wire:click='button'>button</button>
+    <b>{{ $sumar }}</b>
+</div>
